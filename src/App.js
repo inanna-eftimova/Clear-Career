2072fb8504c44948c0e3d2743a338ac9c6b01dc3
inanna-eftimova <eftimovainana@gmail.com>
2023-09-06 22:07:53 +0300

    Added router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
+import { Route, Routes } from "react-router-dom";
+
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer"
 import HomePage from "./components/Pages/HomePage";
+import RegisterPage from "./components/Pages/RegisterPage"
+import LoginPage from "./components/Pages/LoginPage";
+import CreatePage from "./components/Pages/Offers/CreateOfferPage";
+import DashboardPage from "./components/Pages/DashboardPage";
 
 function App() {
   return (
     <div>
-    <div id="wrapper">
-      <Header />
-      <main>
-        <HomePage />
+      <div id="wrapper">
+        <Header />
+        <main>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/create" element={<CreatePage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+          </Routes>
+          <HomePage />
 
-      </main>
-    </div>
-    <Footer/>
+        </main>
+      </div>
+      <Footer />
     </div>
   );
 }
